c136805ac8fb91defebd36637f70036012c8e4b7
Karol Kozakowski <github@karolkozakowski.io>
2024-09-29 20:23:37 +0200

    Format home file

--- nvim/config/lua/nvim-tree-config.lua
+++ nvim/config/lua/nvim-tree-config.lua
@@ -1,43 +1,11 @@
 require'nvim-tree'.setup {
-    disable_netrw       = true,
-    hijack_netrw        = true,
-
-    hijack_directories   = {
-        enable = true,
-        auto_open = true,
-    },
-
-    open_on_tab         = false,
-    hijack_cursor       = false,
-    update_cwd          = true,
-    diagnostics         = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-        }
-    },
-
-    update_focused_file = {
-        enable      = true,
-        update_cwd  = true,
-        ignore_list = {}
-    },
-
-    system_open = {
-      cmd  = nil,
-      args = {}
-    },
-
-    git = {
-      enable = true
-    },
-
-    actions = {
-        open_file = {
-            resize_window = true
-        }
-    }
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
 }
