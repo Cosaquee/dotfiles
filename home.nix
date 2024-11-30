c847babe3b69ed7b9af981f7ff10d7f3e04e4d00
Karol Kozakowski <github@karolkozakowski.io>
2024-11-30 13:15:09 +0100

    Update settings

--- home.nix
+++ home.nix
@@ -164,8 +164,6 @@
 
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
     gpgconf --launch gpg-agent
-
-    eval "$(direnv hook zsh)"
   '';
 
   programs.vscode = {
--- nvim/config/lua/settings.lua
+++ nvim/config/lua/settings.lua
@@ -1,20 +1,32 @@
 local opt = vim.opt
 local g = vim.g
 
--- global options --
-opt.incsearch = true    -- Find the next match as we type the search
-opt.hlsearch = true     -- Hilight searches by default
-opt.viminfo = "'100,f1" -- Save up to 100 marks, enable capital marks
-opt.ignorecase = true   -- Ignore case when searching...
-opt.smartcase = true    -- ...unless we type a capital
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.number = true
+opt.title = true
+opt.hlsearch = true
+opt.showcmd = true
+opt.cmdheight = 1
+opt.laststatus = 3
+
 opt.autoindent = true
 opt.smartindent = true
 opt.expandtab = true
-opt.termguicolors = true
-opt.cursorline = true
-opt.relativenumber = false
-opt.number = true
-opt.signcolumn="yes:2"
+opt.smarttab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+
+opt.scrolloff = 10
+opt.wrap = false
+
+opt.ignorecase = true
+opt.inccommand = "split"
+
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "cursor"
 
 -- Set leader key
 g.mapleader = ' '
