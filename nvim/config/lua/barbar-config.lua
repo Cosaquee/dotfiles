f8ad1eea47add0ca4988d3dc62ad3fee67c48d75
Karol Kozakowski <github@karolkozakowski.io>
2025-05-08 16:05:43 +0200

    Barbar config

--- nvim/config/lua/barbar-config.lua
+++ nvim/config/lua/barbar-config.lua
@@ -12,3 +12,18 @@ keymap.set("n", "<leader>]", "<cmd>BufferNext<cr>", {
   silent = true,
   desc = "Switch to next buffer",
 })
+
+keymap.set("n", "<leader>bd", "<cmd>BufferClose<cr>", {
+  silent = true,
+  desc = "Close buffer",
+})
+
+keymap.set("n", "<leader>bp", "<cmd>BufferPin<cr>", {
+  silent = true,
+  desc = "Pin buffer",
+})
+
+keymap.set("n", "<leader>bl", "<cmd>BufferCloseAllButCurrentOrPinned<cr>", {
+  silent = true,
+  desc = "Close all but current or pinned buffer",
+})
